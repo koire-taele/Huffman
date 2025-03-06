590ea5822aec9b39730cc42f85df6853f8068716
grialem <galemasov@gmail.com>
2025-03-07 04:44:50 +0700

    Complete overhaul

--- huffman.cpp
+++ huffman.cpp
@@ -2,244 +2,292 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <queue>
 #include <vector>
-#include <unordered_map>
 #include <string>
-#include <bitset>
-#include <map>
+#include <algorithm>
+#include <clocale>
+#include <Windows.h>
 using namespace std;
 
-struct Node
-{
-    char symbol;
-    unsigned long long frequency;
-    Node *left;
-    Node *right;
-}; // Узел дерева
+// реализовано по алгоритму из предложенной преподавателем книги
 
-struct Compare
+bool tabSort(const pair<vector<unsigned char>, int> & freqA, const pair<vector<unsigned char>, int> & freqB)
 {
-    bool operator ()(Node* l, Node* r)
-    {
-        return ((r -> frequency) < (l -> frequency));
-    }
-}; // Используем priority_queue для создания дерева, потому для него задаём свой собственный Compare
-
-Node* CreateNode(char sym = ' ', unsigned long long fr = 0, Node* l = NULL, Node* r = NULL)
-{
-    Node *node = new Node();
-    node -> symbol = sym;
-    node ->frequency = fr;
-    node -> left = l;
-    node -> right = r;
-    return node;
-} // Создание очередного узла
-
-Node* createTree(priority_queue<Node*, vector<Node*>, Compare> nodeList)
-{
-    // Заполняем дерево, пока не выйдем в его корень
-    Node* root = CreateNode();
-    if (nodeList.size() == 0) return root;
-    if (nodeList.size() == 1)
-    {
-        Node* theOnly = nodeList.top();
-        root = CreateNode(' ', theOnly->frequency, theOnly, NULL);
-        return root;
-    }
-    while (nodeList.size() != 1)
-    {
-        Node* l = nodeList.top(); nodeList.pop();
-        Node* r = nodeList.top(); nodeList.pop();
-        nodeList.push(CreateNode(' ', ((l->frequency) + (r->frequency)), l, r));
-    }
-    root = nodeList.top();
-    return root;
+    return freqA.second > freqB.second;
 }
 
-void HuffmanTab(Node* root, unordered_map<char, string> & hCodes, string code)
+bool freqSort(const pair<vector<int>, int> & freqA, const pair<vector<int>, int> & freqB)
 {
-    if (root == NULL) return;
-    if (root->left == NULL && root->right == NULL)
+    return freqA.second > freqB.second;
+}
+
+vector<unsigned char> UTF8_Handler(ifstream & in, char & symbol)
+{
+    unsigned char handler; vector<unsigned char> charSet; // основная идея: хранить байты UTF-8 в наборе char-объектов
+    handler = symbol;
+    charSet.push_back(handler);
+    if (handler < 128) return charSet; // эквивалентно проверке на соответствие шаблону 0xxxxxxx для UTF-8
+    if (handler >= 192 || handler < 224) // эквивалентно проверке на соответствие шаблону 110xxxxx для UTF-8
     {
-        if (code == "")
+        in.get(symbol);
+        handler = symbol;
+        charSet.push_back(handler);
+        return charSet;
+    }
+    if (handler >= 224 || handler < 240) // эквивалентно проверке на соответствие шаблону 1110xxxx для UTF-8
+    {
+        for (int i = 0; i < 2; i++)
         {
-           hCodes[root->symbol] = "0";
-           return; 
+            in.get(symbol);
+            handler = symbol;
+            charSet.push_back(handler);
         }
-        hCodes[root->symbol] = code;
+        return charSet;
     }
-    HuffmanTab(root->left, hCodes, code + "0");
-    HuffmanTab(root->right, hCodes, code + "1");
-}
-
-void writeBin(ofstream & fout, int num) // Для записи частот в файл
-{
-    unsigned char byte = 0, marker = 1; int lim = 0; // unsigned char — тип данных, под переменную которого выделяется 8 бит = 1 байт
-    bitset<32> bnum(num); string bstr = bnum.to_string(); int len = bstr.length();
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < 3; i++) // если не прошла ни одна из проверок выше, то байт соответствует шаблону 11110xxx для UTF-8
     {
-        byte = byte << 1;
-        if (bstr[i] == '1') byte = byte | marker;
-        lim++;
-        if (lim == 8)
+        in.get(symbol);
+        handler = symbol;
+        charSet.push_back(handler);
+    }
+    return charSet;
+} // потом будем использовать эту функцию, чтобы аккуратно переносить UTF-8-символы в таблицу Хаффмана при кодировании и обратно в итоговый файл при декодировании
+
+void encoder(ifstream & in, ofstream & out)
+{
+    vector<pair<vector<unsigned char>, int>> frequencies; // таблица частот: ключ - символ, значение - частота символа
+    bool isThere = false;
+    if (in.is_open())
+    {
+        char symbol, symbolToUpdate; int valueToUpdate; vector<unsigned char> utf8char;
+        in.get(symbol);
+        utf8char = UTF8_Handler(in, symbol);
+        frequencies.push_back(pair<vector<unsigned char>, int>(utf8char, 1));
+        while (in.get(symbol))
         {
-            lim = 0;
-            fout.put(byte);
-            byte = 0;
+            utf8char = UTF8_Handler(in, symbol);
+            isThere = false;
+            for (int i = 0; i < frequencies.size(); i++)
+            {
+                if (utf8char == frequencies[i].first)
+                {
+                    frequencies[i].second++;
+                    isThere = true;
+                    break;
+                }
+            }
+            if (!isThere)
+            {
+                frequencies.push_back(pair<vector<unsigned char>, int>(utf8char, 1));
+            }
+        } // получаем таблицу частот
+        if (frequencies.empty())
+        {
+            cout << "Error: frequencies tab is empty. The file itself is empty or there's a bug in the code." << endl;
+            exit(1);
         }
-    }
-}
-
-int readBin(ifstream & fin) // Для чтения частот из файла
-{
-    string bstr = "";
-    for (int i = 0; i < 4; i++) // записываем 4 куска по 8 бит, всего 32 бит
-    {
-        unsigned char bit_raw = fin.get();
-        bitset<8> bits(bit_raw);
-        bstr += bits.to_string();
-    }
-    bitset<32> result(bstr);
-    return result.to_ullong();
-}
-
-// Запись закодированных данных в файл
-void encodedInBin(const string & encoded, ofstream & fout, map<char, int> hTab)
-{
-    unsigned char byte = 0, marker = 1; int lim = 0;
-    ofstream bof("buffer.txt", ios::binary);
-    for (size_t i = 0; i < encoded.size(); i++)
-    {
-        byte = byte << 1;
-        if (encoded[i] == '1')
-            byte = byte | marker;   
-        lim++;
-        if (lim == 8)
+        sort(frequencies.begin(), frequencies.end(), tabSort); // сортируем по частотам в порядке убывания
+        int size = frequencies.size();
+        vector<vector<int>> indexes;
+        // хранение индексов в таком формате позволяет рассматривать каждый вектор из набора векторов
+        // как набор индексов, для соответствующих символов которых при сложении частот надо обновить коды
+        for (int i = 0; i < size; i++)
         {
-            bof.put(byte);
-            byte = 0;
-            lim = 0;
+            vector<int> another_i = {i};
+            indexes.push_back(another_i);
         }
-    }
-    if (lim > 0) // Запись обрубков в файл
-    {
-        byte = byte << (8 - lim);
-        bof.put(byte);
-    }
-    bof.close();
-    ifstream bif("buffer.txt", ios::binary);
-    fout << lim;
-    for (auto elem: hTab) // Запись частот в файл
-    {
-        if (elem.first == '\n')
+        vector<string> codes; // хранит в себе постепенно формируемые коды
+        for (int i = 0; i < size; i++)
         {
-            fout << '|';
-            writeBin(fout, elem.second);
+            codes.push_back("");
+        }
+
+        // по алгоритму, предложенному в книге, складываются вероятности два последних элементов в таблице,
+        // т.к. таблица отсортирована, то эти два элемента - два последних элемента в таблице
+
+        vector<pair<vector<int>, int>> freqIndexes; // для сопоставления сумм частот и индексов символов, чьи частоты складывались
+        for (int i = 0; i < size; i++)
+        {
+            vector<int> another_i = {i};
+            freqIndexes.push_back(pair<vector<int>, int>(another_i, frequencies[i].second));
+        }
+
+        int currFreqISize = freqIndexes.size();
+        while (currFreqISize > 2)
+        {
+            vector<int> preLastIndexes = freqIndexes[currFreqISize-2].first;
+            vector<int> LastIndexes = freqIndexes[currFreqISize-1].first;
+            int preLastFreqs = freqIndexes[currFreqISize-2].second;
+            int LastFreqs = freqIndexes[currFreqISize-1].second; // берём последнюю и предпоследнюю строки в таблице
+            
+            // приписываем 0 и 1 тем символам, которые учавствовали в данной сумме (индексы здеь отображают соответствующие им символы)
+            for (int stringI = 0; stringI < preLastIndexes.size(); stringI++)
+            {
+                codes[preLastIndexes[stringI]] = "0" + codes[preLastIndexes[stringI]];
+            }
+            for (int stringI = 0; stringI < LastIndexes.size(); stringI++)
+            {
+                codes[LastIndexes[stringI]] = "1" + codes[LastIndexes[stringI]];
+            }
+
+            int newFreq = preLastFreqs + LastFreqs;
+            vector<int> newIndexes = preLastIndexes;
+            for (int i = 0; i < LastIndexes.size(); i++) newIndexes.push_back(LastIndexes[i]);
+            pair<vector<int>, int> newMember = {newIndexes, newFreq};
+            freqIndexes.erase(freqIndexes.begin() + (freqIndexes.size()-1));
+            freqIndexes.erase(freqIndexes.begin() + (freqIndexes.size()-1)); // стираем два последних элемента таблицы
+            freqIndexes.push_back(newMember); // и добавляем новый
+            sort(freqIndexes.begin(), freqIndexes.end(), freqSort); // таблица всегда должна быть отсортирована
+            currFreqISize = freqIndexes.size();
+        }
+        if (size >= 1) // последняя итерация обновления кодов для символов, одновременно проверка на случай, если текст имел только один символ или два различных
+        {
+            for (int stringI = 0; stringI < freqIndexes[0].first.size(); stringI++)
+            {
+                codes[freqIndexes[0].first[stringI]] = "0" + codes[freqIndexes[0].first[stringI]];
+            }
+        }
+        if (size >= 2)
+        {
+            for (int stringI = 0; stringI < freqIndexes[1].first.size(); stringI++)
+            {
+                codes[freqIndexes[1].first[stringI]] = "1" + codes[freqIndexes[1].first[stringI]];
+            }
+        }
+
+        vector<pair<vector<unsigned char>, string>> huffmanTab; // составление таблицы Хаффмана
+        for (int i = 0; i < size; i++) huffmanTab.push_back(pair<vector<unsigned char>, string>(frequencies[i].first, codes[i]));
+        in.close();
+        in.open("text.txt");
+        int index;
+        int textLength = 0;
+        string result = ""; // составление закодированного текста
+        while (in.get(symbol))
+        {
+            utf8char = UTF8_Handler(in, symbol);
+            index = 0;
+            while (utf8char != huffmanTab[index].first) index++;
+            result += huffmanTab[index].second;
+            textLength++;
+        }
+        
+        out << textLength << ' ' << result.length() << ' '; // запись длины текста оригинального и зашифрованного, чтобы потом правильно декодировать
+        
+        // запись таблицы Хаффмана в файл; в первой строке пишем кол-во строк в таблице, чтобы потом верно считать её из файла с закодированным текстом
+        out << size << endl;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < huffmanTab[i].first.size(); j++) out.put(huffmanTab[i].first[j]);
+            out << huffmanTab[i].second << endl;
+        }
+
+        // далее запись самого закодированного текста
+        int encodedLength = result.length();
+        char byte = 0;
+        int byteDiv = 0;
+        char marker = 1;
+        for (int i = 0; i < encodedLength; i++) // С++ не даёт записать один бит в файл, он даёт писать туда минимум 1 байт, поэтому приходится изворачиваться
+        {
+            byte = byte << 1;
+            if (result[i] == '1')
+            {
+                byte |= marker;
+            }
+            byteDiv++;
+            if (byteDiv == 8)
+            {
+                out.put(byte);
+                byte = 0;
+                byteDiv = 0;
+            }
+        }
+        if (byteDiv != 0)
+        {
+            byte = byte << (8 - byteDiv);
+            out.put(byte);
+        }
+        in.close();
+        out.close();
+    }
+};
+
+void decoder(ifstream & in, ofstream & out)
+{
+    // Восстанавливаем данные о длине оригинального и закодированного сообщений, а также таблицу Хаффмана 
+    string data;
+    string toDecode = "";
+    string code;
+    string strTextLength;
+    string strEncodedLength;
+    string strHuffSize;
+    getline(in, data);
+    bool afterSpaceFirst = false; bool afterSpaceSecond = false;
+    for (int i = 0; i < data.length(); i++)
+    {
+        if (data[i] == ' ' && !afterSpaceFirst)
+        {
+            afterSpaceFirst = true;
+            continue;
+        }
+        if (data[i] == ' ' && !afterSpaceSecond)
+        {
+            afterSpaceSecond = true;
+            continue;
+        }
+        if (!afterSpaceFirst)
+        {
+            strTextLength += data[i];
+        }
+        else if (!afterSpaceSecond)
+        {
+            strEncodedLength += data[i];
         }
         else
         {
-            fout << elem.first;
-            writeBin(fout, elem.second);
+            strHuffSize += data[i];
         }
     }
-    fout << "\n";
-    char current;
-    while (bif.get(current)) fout << current;
-    bif.close();
-    remove("buffer.txt");
-}
-
-// Декодирование по таблице Хаффмана
-string hCodesDC(const string & source, const unordered_map<char, string> & hCodes)
-{
-    string result = "", buffer = "";
-    size_t hcSize = hCodes.size(), sSize = source.size();
-    if (hcSize == 1)
+    int textLength = stoi(strTextLength);
+    int encodedLength = stoi(strEncodedLength);
+    int size = stoi(strHuffSize);
+    char symbol;
+    vector<unsigned char> utf8char;
+    vector<pair<vector<unsigned char>, string>> huffmanTab;
+    for (int i = 0; i < size; i++)
     {
-        for (auto p: hCodes) result += p.first;
-        return result;
+        in.get(symbol);
+        utf8char = UTF8_Handler(in, symbol);
+        getline(in, code);
+        huffmanTab.push_back(pair<vector<unsigned char>, string>(utf8char, code));
     }
-    for (size_t i = 0; i < sSize; i++)
+
+    unsigned char marker; unsigned char u_symbol; unsigned char byte;
+    while(in.get(symbol))
     {
-        buffer += source[i];
-        for (auto p: hCodes)
+        u_symbol = symbol;
+        marker = 128;
+        for (int i = 0; i < 8; i++)
         {
-            if (p.second == buffer)
+            if (u_symbol & marker) toDecode += '1';
+            else toDecode += '0';
+            marker = marker >> 1;
+        }
+    }
+
+    string another_code;
+    for (int i = 0; i < encodedLength; i++)
+    {
+        another_code += toDecode[i];
+        for (int j = 0; j < size; j++)
+        {
+            if (another_code == huffmanTab[j].second)
             {
-                if (p.first == '|')
-                {
-                    result += '\n';
-                    buffer = ""; 
-                    break;
-                }
-                result += p.first;
-                buffer = "";
+                for (int k = 0; k < huffmanTab[j].first.size(); k++) out.put(huffmanTab[j].first[k]);
+                another_code = "";
                 break;
             }
         }
     }
-    return result;
-}
-
-
-string ReadFreqs(ifstream & file, map<char, int> & frequencies)
-{
-    string result = ""; char current;
-    unsigned char byte, marker = 1;
-    size_t remainings = file.get() - '0';
-    while (file.get(current))
-    {
-        if (current == '|') frequencies['\n'] = readBin(file);
-        else if (current == '\n') break;
-        else frequencies[current] = readBin(file);
-    }
-    while (file.get((char &)byte))
-    {
-        for (int i = 7; i >= 0; i--)
-        {
-            unsigned char buffer = (byte >> i);
-            if (buffer & marker) result += '1';
-            else result += '0';
-        }
-    }
-    result = result.substr(0, result.size() - (8 - remainings));
-    return result;
-}
-
-void hEncode(ifstream& in, ofstream& out) // функция кодировки, содержит в себе формирование частот,
-                                          // создание дерева и использование функций для кодирования
-{
-    string source, result; map<char, int> frequencies;
-    priority_queue<Node*, vector<Node*>, Compare> nodeInfo;
-    unordered_map<char, string> huffmansTab;
-    char symbol;
-    while (in.get(symbol)) source += symbol;
-    size_t sSize = source.size();
-    for (size_t i = 0; i < source.size(); i++) frequencies[source[i]]++;
-    for (auto p: frequencies) nodeInfo.push(CreateNode(p.first, p.second, NULL, NULL));
-    Node *tree = createTree(nodeInfo);
-    HuffmanTab(tree, huffmansTab, "");
-    for (size_t i = 0; i < sSize; i++) result += huffmansTab[source[i]];
-    encodedInBin(result, out, frequencies);
-    in.close();
-    out.close();
-}
-
-
-void hDecode(ifstream& in, ofstream& out) // функция декодировки
-{
-    string source = "";
-    string result = "";
-    unordered_map<char, string> huffmansTab;
-    map<char, int> frequencies;
-    priority_queue<Node*, vector<Node*>, Compare> nodeInfo;     
-    source = ReadFreqs(in, frequencies);
-    for (auto p: frequencies) nodeInfo.push(CreateNode(p.first, p.second, NULL, NULL));
-    Node* tree = createTree(nodeInfo);
-    HuffmanTab(tree, huffmansTab, "");
-    result = hCodesDC(source, huffmansTab);
-    out << result;
     in.close();
     out.close();
 }
@@ -257,33 +305,30 @@ bool mode()
     }
 }
 
-
 int main()
 {
     bool codeOrDecode = mode();
     if (!codeOrDecode)
     {
-        ifstream input("text.txt", ios::binary);
-        ofstream output("resultE.bin", ios::binary);
-        if (!input.is_open() || !output.is_open())
+        ifstream einput("text.txt", ios::in);
+        ofstream eoutput("resultE.bin", ios::out | ios::binary);
+        if (!einput.is_open() || !eoutput.is_open())
         {
             cout << "Error: can't open file." << endl;
             exit(1);
         }
-        hEncode(input, output);
+        encoder(einput, eoutput);
         cout << "Encoding done. Result is saved in resultE.bin." << endl;
     }
     else
     {
-        ifstream input("resultE.bin", ios::binary);
-        ofstream output("resultD.txt", ios::binary);
-        if (!input.is_open() || !output.is_open())
-        {
+        ifstream dinput("resultE.bin", ios::binary);
+        ofstream doutput("resultD.txt", ios::binary);
+        if (!dinput.is_open() || !doutput.is_open()) {
             cout << "Error: can't open file." << endl;
             exit(1);
         }
-        hDecode(input, output);
-        cout << "Decoding done. Result is saved in resultD.txt." << endl;
+        decoder(dinput, doutput);
+        cout << "Decoding done. Result is saved in resultD.txt." << endl;        
     }
-    return 0;
 }
